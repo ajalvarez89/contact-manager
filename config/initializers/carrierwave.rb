f8c7da0b0c613eecff9f0f243a4fbf7efed468bd
Alvaro Alvarez <alvaro.alvarez@monokera.com>
2021-03-07 02:23:53 -0500

    security env improve

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     :provider => 'AWS',
-    :aws_access_key_id => 'AKIARWRMIH3UF353WXVW',
-    :aws_secret_access_key => '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'
+    :aws_access_key_id => ENV["AWS_ACCESS_KEY_ID"],
+    :aws_secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]
   }
   config.fog_directory = 'ajalvarez'
   config.storage = :fog
